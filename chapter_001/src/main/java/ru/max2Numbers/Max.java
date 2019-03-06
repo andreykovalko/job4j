b52d9d69f2af211d1f562769b81b55c9ce56361b
andreykovalko <kovalkoandrey1983@mail.ru>
2019-03-06 20:37:11 +0300

    4.2. Maximum tw0 numbers[#116332]

---
+++ chapter_001/src/main/java/ru/max2Numbers/Max.java
@@ -0,0 +1,19 @@
+package ru.max2Numbers;
+
+/**
+ * @author Andrey Kovalko (kovalkoandrey1983@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Method max
+     * Evaluation maximum two numbers.
+     * @param firstNumber, secondNumber.
+     * @return maximum.
+     */
+    public int max (int firstNumber, int secondNumber) {
+        int maximum = firstNumber > secondNumber ? firstNumber : secondNumber;
+        return maximum;
+    }
+}
---
+++ chapter_001/src/test/java/ru/max2Numbers/MaxTest.java
@@ -0,0 +1,39 @@
+package ru.max2Numbers;
+
+        import org.junit.Test;
+        import static org.hamcrest.core.Is.is;
+        import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Andrey Kovalko (kovalkoandrey1983@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MaxTest {
+    /**
+     * Test max().
+     */
+    @Test
+    public void whenMax1To2Then2() {
+        Max maxNumber = new Max();
+        int result = maxNumber.max(1,2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax2To1Then2() {
+        Max maxNumber = new Max();
+        int result = maxNumber.max(2, 1);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax2To2Then2() {
+        Max maxNumber = new Max();
+        int result = maxNumber.max(1,1);
+        assertThat(result, is(1));
+    }
+
+}
