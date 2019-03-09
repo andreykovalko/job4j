2eb72262fd78806f11583df3dfe24d332a7e2f83
andreykovalko <kovalkoandrey1983@mail.ru>
2019-03-09 21:23:09 +0300

    5.3. Constructing chess board in pseudography.[#116343]

---
+++ chapter_001/src/main/java/ru/loop/Board.java
@@ -0,0 +1,31 @@
+package ru.loop;
+
+/**
+ * @author Andrey Kovalko (kovalkoandrey1983@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Board {
+    /**
+     * Method paint.
+     * Painting chess board.
+     * @param width width of chess board.
+     * @param height height of chess board.
+     * @return screen.
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((i + j) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
