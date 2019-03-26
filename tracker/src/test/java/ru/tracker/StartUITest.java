64aba25d3ac9da08400954a0a29de555efadf246
andreykovalko <kovalkoandrey1983@mail.ru>
2019-03-26 09:58:35 +0300

    3.Write tests for checking user behavior using class StubInput[#116407]

---
+++ tracker/src/test/java/ru/tracker/StartUITest.java
@@ -0,0 +1,38 @@
+package ru.tracker;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+
+public class StartUITest {
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();     // создаём Tracker
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc", new Date()));
+        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
+    }
+
+    @Test
+    public void whenDeleteThenTrackerHasNotValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc", new Date()));
+        Input input = new StubInput(new String[]{"3", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+}
